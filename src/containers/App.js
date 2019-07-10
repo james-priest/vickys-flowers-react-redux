16bcb41289c764ab646531ce60a639e00f50a82b
James Priest <james-priest@outlook.com>
2019-07-10 02:26:15 -0700

    feat: Connect to store and populate from API

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from '../components/Header'
 import ProductList from '../components/ProductList'
 import Footer from '../components/Footer'
+import { handleInitialData } from '../actions/shared'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <>
-      <Header />
-      <div className="scroll-container">
-        <ProductList />
-        <Footer />
-      </div>
-    </>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData()
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="scroll-container">
+          <ProductList />
+          <Footer />
+        </div>
+      </>
+    )
+  }
 }
 
-export default App
+export default connect(
+  null,
+  { handleInitialData }
+)(App)
