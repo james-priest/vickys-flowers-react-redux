bcb8ac015731ad037644a75211046dcece555b71
James Priest <james-priest@outlook.com>
2019-07-10 02:24:04 -0700

    fix: invoke functions in Promise.all

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,14 @@
 import { _getProducts, _getCart, _saveCart } from './_DATA'
 
+// export function getInitialData() {
+//   return Promise.all([_getProducts(), _getCart()]).then(([products, cart]) => ({
+//     products,
+//     cart
+//   }))
+// }
+
 export async function getInitialData() {
-  const [products, cart] = await Promise.all([_getProducts, _getCart])
+  const [products, cart] = await Promise.all([_getProducts(), _getCart()])
   return { products, cart }
 }
 
