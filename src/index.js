4043c4f8a27c045dbf13d08a10f63a4bfdaba94f
James Priest <james-priest@outlook.com>
2019-07-10 02:25:31 -0700

    feat: Add react-redux Provider & create store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './containers/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/root'
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
